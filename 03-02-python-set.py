2f7f3024d670a32e08887627e189301319505573
安藤貴章 <andoutakaaki@andoutakaakinoMacBook-ea.local>
2018-10-09 13:50:34 +0900

    0302

--- 03-02-python-set.py
+++ 03-02-python-set.py
@@ -1,27 +1,30 @@
-## 授業前課題_Python 演習_2
-
-
 course_dict = {
-     'AIコース': {'Aさん', 'Cさん', 'Dさん'},
-     'Railsコース': {'Bさん', 'Cさん', 'Eさん'},
-     'Railsチュートリアルコース': {'Gさん', 'Fさん', 'Eさん'},
+    'AIコース': {'Aさん', 'Cさん', 'Dさん'},
+    'Railsコース': {'Bさん', 'Cさん', 'Eさん'},
+    'Railsチュートリアルコース': {'Gさん', 'Fさん', 'Eさん'},
     'JS': {'Aさん', 'Gさん', 'Hさん'},
-     }
+}
 
-        def find_person(want_to_find_person):
-        """
-        受講生がどのコースに在籍しているかを出力する。
-        まずはフローチャートを書いて、どのようにアルゴリズムを解いていくか考えてみましょう。
-        """
-        #a = 'want_to_find_person' & 'AIコース'
-         b = 'want_to_find_person' & 'Railsコース'
-         c = 'want_to_find_person' & 'Railsチュートリアルコース'
-         d = 'want_to_find_person' & 'JS'
 
-        def main():
-        want_to_find_person = {'Cさん', 'Aさん'}
-        print('探したい人: {}'.format(want_to_find_person))
-        find_person(want_to_find_person)
+def find_person(want_to_find_person):
+    """
+    受講生がどのコースに在籍しているかを出力する。
+    まずはフローチャートを書いて、どのようにアルゴリズムを解いていくか考えてみましょう。
+    """
+    # ここにコードを書いてみる
+def main():
+   want_to_find_person = {'Cさん', 'Aさん'}
+   print('探したい人: {}'.format(want_to_find_person))
+   find_person(want_to_find_person)
 
-        if __name__ == '__main__':
-        main()
+   for key in course_dict.keys():
+    if want_to_find_person <= course_dict[key]:
+      print(key+'に'+str(want_to_find_person)+'は在籍しています。')
+    elif len(want_to_find_person & course_dict[key]) > 0:
+      print(key+'に'+str(want_to_find_person & course_dict[key])
+            +'のみ在籍しています。')
+    else:
+       print(key+'に'+str(want_to_find_person)+'は在籍していません。')
+
+if __name__ =='__main__':
+   main()
